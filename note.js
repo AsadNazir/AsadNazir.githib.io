b9ff826d9706f841f93d58de9ac49bb1703aaf2d
AsadNazir <79693635+AsadNazir@users.noreply.github.com>
2021-05-02 02:35:08 +0500

    Add files via upload

--- note.js
+++ note.js
@@ -3,22 +3,29 @@ let btn = document.querySelector('.btn');
 let note_no = 1;
 let text;
 let new_note;
+let obj;
 let del_all = document.querySelector('.Del_all');
 show_note();
 btn.addEventListener('click', function () {
 
     let local = localStorage.getItem('notes');
     let textarea = document.querySelector('#text')
-
+    let head = document.querySelector('#head');
     if (local == null) {
         notesObj = [];
     }
     else {
         notesObj = JSON.parse(local);
     }
-    notesObj.push(textarea.value);
+    obj = {
+        title: head.value,
+        text: textarea.value,
+        state: false,
+    }
+    notesObj.push(obj);
     localStorage.setItem('notes', JSON.stringify(notesObj));
     textarea.value = "";
+    head.value = "";
     show_note();
 
 })
@@ -37,18 +44,37 @@ function show_note() {
     }
     if (notesObj.length != 0) {
         notesObj.forEach(function (ele, index) {
-            html += ` <div class="card">
-        <h3>Note ${index+1}</h3>
-        <div class="text">
-            <p id="note">${ele}</p>
-        </div>
-        <button class="btn btn-red id="${index}" onclick="delet_btn(this.id)">Delete</button>
-    </div>`;
+            if (ele.state == true) {
+                html += ` <div class="card btn-border border">
+                <h3>${ele.title}</h3>
+                <div class="text">
+                    <p id="note">${ele.text}</p>
+                </div>
+                <div class="btn-class">
+                <button class="btn btn-red" id="${index}" onclick="delet_btn(this.id)">Delete</button>
+                <button class="btn btn-yellow" id="${index}" onclick="unmark_btn(this.id)">Unmark</button>
+                </div>
+            </div>`;
+
+            }
+            else {
+
+                html += ` <div class="card border">
+                  <h3>${ele.title}</h3>
+                  <div class="text">
+                <p id="note">${ele.text}</p>
+               </div>
+               <div class="btn-class">
+                <button class="btn btn-red" id="${index}" onclick="delet_btn(this.id)">Delete</button>
+                <button class="btn btn-yellow" id="${index}" onclick="mark_btn(this.id)">Mark</button>
+                </div>
+                </div>`;
+            }
         }
         )
     }
     else /*(notesObj.length != 0)*/ {
-        html = `<div class="card" style="text-align:center;">
+        html = `<div class="card border" style="text-align:center;">
             <h1>Add a Note here !</h1>
              <div class="text">
              <p>No note to display here</p>
@@ -62,9 +88,9 @@ function show_note() {
 
 
 del_all.addEventListener('click', function () {
+
     let local = localStorage.getItem('notes');
-    if (local != null) 
-    {
+    if (local != null) {
         localStorage.clear();
     }
     notesObj = [];
@@ -76,20 +102,20 @@ del_all.addEventListener('click', function () {
 
 
 
-function show_empty() {
-    let cont = document.querySelector('.cont2')
-    cont.innerHTML = `<div class="card" style="text-align:center;">
-    <h1>Add a Note here !</h1>
-    <div class="text">
-        <p>No note to display here</p>
-    </div>
-</div>`
-}
+// function show_empty() {
+//     let cont = document.querySelector('.cont2')
+//     cont.innerHTML = `<div class="card" style="text-align:center;">
+//     <h1>Add a Note here !</h1>
+//     <div class="text">
+//         <p>No note to display here</p>
+//     </div>
+// </div>`
+// }
 
 
-
+//-------------------------------------------------
+// Delete btn
 function delet_btn(index) {
-    console.log("delete");
     let local = localStorage.getItem('notes');
     if (local == null) {
         notesObj = [];
@@ -104,4 +130,47 @@ function delet_btn(index) {
     show_note();
 
 }
+//------------------------------------------------------
+// serching elememt in the note app
+let serach = document.querySelector('#search');
+search.addEventListener('input', function () {
 
+    let txt = serach.value;
+    let notecrads = document.querySelectorAll(".card");
+    Array.from(notecrads).forEach(function (ele) {
+        let cardtxt = ele.getElementsByTagName("p")[0].innerText;
+
+        if (cardtxt.includes(txt)) {
+            ele.style.display = "";
+
+        }
+        else {
+
+            ele.style.display = "none";
+        }
+    }
+
+    )
+
+})
+//------------------------------------------------
+//mark_btn function
+function mark_btn(index) {
+    // console.log(notesObj[index]);
+    let newobj = notesObj[index];
+    newobj.state = true;
+    notesObj[index] = newobj;
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    show_note();
+   
+}
+
+// unmark_btn function
+function unmark_btn(index){
+    // console.log(notesObj[index]);
+    let newobj = notesObj[index];
+    newobj.state = false;
+    notesObj[index] = newobj;
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    show_note();
+}
